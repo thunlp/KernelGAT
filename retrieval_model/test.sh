cc9398c5ffbf0034725cd5818dc305e003b53782
EdwardZH <402151242@qq.com>
2019-11-05 15:02:17 +0800

    update

--- retrieval_model/test.sh
+++ retrieval_model/test.sh
@@ -20,4 +20,4 @@ python test.py --outdir ./output/ \
 
 python process_data.py --retrieval_file ./output/train.json --gold_file ../data/golden_train.json --output ../data/bert_train.json
 python process_data.py --retrieval_file ./output/dev.json --gold_file ../data/golden_dev.json --output ../data/bert_dev.json
-python process_data.py --retrieval_file ./output/test.json --gold_file ./output/test.json  --output ../data/bert_test.json --test
+python process_data.py --retrieval_file ./output/test.json --gold_file ./output/all_test.json  --output ../data/bert_test.json --test
